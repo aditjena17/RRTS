322610a998b6b9a0cffae7b972a4afd97c278bfe
adit <aditjena17@gmail.com>
2025-03-25 13:34:03 +0530

    changed some logic

--- src/main/java/com/RRTS/RRTS/classes/Todo.java
+++ src/main/java/com/RRTS/RRTS/classes/Todo.java
@@ -48,14 +48,6 @@ public class Todo {
     	HIGH
     }
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public String getTitle() {
 		return title;
 	}
